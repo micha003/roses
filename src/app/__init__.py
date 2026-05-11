cec1331ba4a5dce5c2d339ef0ca2d1545b439d43
micha003 <michael.vorogushyn@wbgym.de>
2026-05-11 19:26:58 +0200

    core stuff

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, app
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+import os
+import dotenv
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") or os.urandom(24)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///roses.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    from app.routes.auth import auth
+    from app.routes.roses import roses
+
+    app.register_blueprint(auth)
+    app.register_blueprint(roses, url_prefix="/roses")
 
     return app
